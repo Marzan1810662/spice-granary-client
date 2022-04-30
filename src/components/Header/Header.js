d024e1436d5f1a0b98e48fdf3deb81eb1bec0adb
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-30 16:02:26 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <Navbar className='navbar' collapseOnSelect expand="lg" >
+            <Container>
+                <Navbar.Brand href="#home">
+                    <h1 className='fw-bold mb-0'>Spice Granary</h1>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto fs-5">
+                        <Nav.Link className='pe-3' href="#home">Home</Nav.Link>
+                        <NavDropdown className='pe-3'
+                            title="Profile" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Manage Items</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Add Items</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">My Items</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
