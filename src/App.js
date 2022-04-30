91a5bfeb2a0d110a4f86adcb25ff2af49abbf0ea
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-30 21:15:56 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className='not-found-page-container'>
+            <div className='not-found-content-container'>
+                <h1 className=''>404</h1>
+                <h3 className=' fs-3'>Page not Found!</h3>
+                <h5 className=' fs-3'>The page you are looking for does not exist or an error occurred</h5>
+                <Link to='/'><button className='back-to-home-btn'>Go Back Home</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
