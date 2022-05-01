3c2ac77fc74c540abe5dd52236234e913dd793dc
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-02 00:49:53 +0600

    sign-in with Gogle implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { NavLink } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Header.css';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
     return (
         <Navbar className='navbar' collapseOnSelect expand="lg" >
             <Container>
@@ -14,15 +17,18 @@ const Header = () => {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto fs-5">
                         <Nav.Link className='pe-3' as={NavLink} to="/home">Home</Nav.Link>
-                        <Nav.Link className='pe-3' as={NavLink} to="/login">Login</Nav.Link>
-                        <NavDropdown className='pe-3'
-                            title="Profile" id="collasible-nav-dropdown">
-                            <NavDropdown.Item as={NavLink} to="/action/3.1">Manage Items</NavDropdown.Item>
-                            <NavDropdown.Item as={NavLink} to="/action/3.2">Add Items</NavDropdown.Item>
-                            <NavDropdown.Item as={NavLink} to="/action/3.3">My Items</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item as={NavLink} to="/action/3.4">Logout</NavDropdown.Item>
-                        </NavDropdown>
+                        {user ?
+                            <NavDropdown className='pe-3'
+                                title="Profile" id="collasible-nav-dropdown">
+                                <NavDropdown.Item as={NavLink} to="/manageItems">Manage Items</NavDropdown.Item>
+                                <NavDropdown.Item as={NavLink} to="/addItems">Add Items</NavDropdown.Item>
+                                <NavDropdown.Item as={NavLink} to="/myItems">My Items</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item as={NavLink} to="/action/3.4">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                            :
+                            <Nav.Link className='pe-3' as={NavLink} to="/login">Login</Nav.Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,9 +1,28 @@
 import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import './SocialLogin.css';
 
 const SocialLogin = () => {
+    const [error, setError] = useState();
+    const navigate = useNavigate();
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (googleLoading) {
+        return <p>loading ...</p>
+    }
+
+    let authErrorElement;
+    if (googleError) {
+        authErrorElement = <p className='text-danger'>Error: {googleError.message}</p>
+    }
+
+    if (googleUser) {
+        navigate('/');
+    }
     return (
         <div className='social-login-container'>
             <div className='d-flex align-items-center '>
@@ -11,8 +30,9 @@ const SocialLogin = () => {
                 <h6>Or</h6>
                 <span></span>
             </div>
+            {authErrorElement}
             <div className='d-flex flex-column'>
-                <button className='google-login-btn my-2'><FontAwesomeIcon icon={faGoogle} /> Continue With Google</button>
+                <button onClick={() => signInWithGoogle()} className='google-login-btn my-2'><FontAwesomeIcon icon={faGoogle} /> Continue With Google</button>
                 <button className='facebook-login-btn my-2'><FontAwesomeIcon icon={faFacebookF} /> Continue With Facebook</button>
             </div>
         </div>
