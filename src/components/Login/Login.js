f44cbb0854a3048fb18aa514d92b51c2574126ee
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-05 21:28:46 +0600

    reset password with email link  implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import React, { useState } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './login.css';
 
 const Login = () => {
     const navigate = useNavigate();
-    const { register, errors, handleSubmit } = useForm();
+    const [emailError, setEmailError] = useState('');
+    const { register, errors, getValues, handleSubmit } = useForm();
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         loginError,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
+        auth
+    );
 
     const onSubmit = (data) => {
-
         const email = data.Email;
         const password = data.Password;
 
         signInWithEmailAndPassword(email, password)
+    };
+
+    const handleResetPassword = async () => {
+        const email = getValues("Email");
+        let emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailPattern.test(email)) {
+            setEmailError('Please enter a valid email address');
+            return;
+        }
+        await sendPasswordResetEmail(email);
+        toast('An email has been sent to your account. Please update your password.', {
+            position: toast.POSITION.BOTTOM_LEFT
+        });
+
     };
 
     if (user) {
@@ -29,8 +48,8 @@ const Login = () => {
     }
 
     let loginErrorElement;
-    if (loginError) {
-        loginErrorElement = <p className='text-danger'>Error:{loginError.message}</p>
+    if (loginError || error) {
+        loginErrorElement = <p className='text-danger'>Error:{loginError?.message}{error?.message}</p>
     }
     return (
         <div className='login-form-main-container d-flex align-items-center 
@@ -46,6 +65,7 @@ const Login = () => {
                             pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                         })}
                     />
+                    <p className='text-danger text-start'>{emailError}</p>
                     <label className='text-start'>Password</label>
                     <input className='main-form-input mb-2'
                         type="password"
@@ -53,11 +73,13 @@ const Login = () => {
                             required: true
                         })}
                     />
+                    <h6 className='mb-3 text-start'>Forgot password?<span className='reset-btn' onClick={handleResetPassword} >Reset</span> </h6>
                     {loginErrorElement}
                     <input className='login-btn my-2' type="submit" value="Login" />
                 </form>
                 <h6 className='mb-3'>Already Have An Account?<span className='register-link'><Link to='/register'>Register Now</Link></span> </h6>
                 <SocialLogin />
+                <ToastContainer />
             </div>
             <div className='img-container'>
             </div>
