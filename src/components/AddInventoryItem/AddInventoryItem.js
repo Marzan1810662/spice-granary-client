283d4fe2204a3964b48ade6b49f7ed25ba1914a6
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-07 22:34:18 +0600

    post new Item to server

--- src/components/AddInventoryItem/AddInventoryItem.js
+++ src/components/AddInventoryItem/AddInventoryItem.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Children } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import swal from 'sweetalert';
+import auth from '../../firebase.init';
 import PageTitle from '../PageTitle/PageTitle';
 import './AddInventoryItem.css';
 
 const AddInventoryItem = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [user, loading, error] = useAuthState(auth);
     const onSubmit = data => {
-        console.log(data)
+        ;
+        const newItem = data;
+        axios.post('https://spice-granary.herokuapp.com/item', newItem)
+            .then(function (response) {
+                const {data} = response;
+               if(data.insertedId){
+                swal({
+                    title: "Success",
+                    text: "Item is Added Sucessfully",
+                    icon: "success",
+                    timer: 2000,
+                    button: false
+                  });
+               }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
-    // watch("picture") && ( )
-    //  console.log(watch("picture"));
-    // console.log(watch("exampleRequired"));
+    console.log(user);
+
     return (
         <div className='add-item-main-container py-4'>
-            <PageTitle  title="Add New Item"/>
+            <PageTitle title="Add New Item" />
             <div className='add-item-form-conatiner container my-5 mx-auto'>
                 <div>
                     <h1 className='add-item-section-title my-3 fw-bold'>Add Inventory Item</h1>
@@ -32,8 +53,8 @@ const AddInventoryItem = () => {
                     <label className='text-start'>Supplier Name</label>
                     <input className='main-form-input mb-2' type="text" {...register("supplierName", { required: true })} />
                     {errors.supplierName && <span className='text-start text-danger'>This field is required</span>}
-                    {/* <label className='text-start'>Email</label>
-                    <input className='main-form-input mb-2' type="email" defaultValue="email" {...register("email", { required: true })} /> */}
+                    <label className='text-start'>Email</label>
+                    <input className='main-form-input mb-2' type="email" defaultValue={user.email} {...register("email", { required: true })} readOnly />
                     <label className='text-start'>Item Quantity</label>
                     <input className='main-form-input mb-2' type="number" min="1" {...register("quantity", { required: true })} />
                     {errors.quantity && <span className='text-start text-danger'>This field is required</span>}
