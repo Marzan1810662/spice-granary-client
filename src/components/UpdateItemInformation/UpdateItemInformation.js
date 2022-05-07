6c6179ab421723b5b312dfc6d150a9e2f6b928d6
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-08 00:25:57 +0600

    get specidfic item from server on update information page

--- src/components/UpdateItemInformation/UpdateItemInformation.js
+++ src/components/UpdateItemInformation/UpdateItemInformation.js
@@ -1,6 +1,7 @@
 import { faListCheck, faShippingFast } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useParams } from 'react-router-dom';
 import PageTitle from '../PageTitle/PageTitle';
@@ -8,33 +9,49 @@ import './UpdateItemInformation.css';
 
 const UpdateItemInformation = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const { id } = useParams();
-    console.log(id);
+    const { id } = useParams(); 
+    const [item, setItem] = useState({});
+
+    useEffect(() => {
+        const url = `https://spice-granary.herokuapp.com/item/${id}`;
+        console.log(url);
+
+        axios.get(url)
+        .then(response => {
+            const {data} = response;
+            console.log(response);
+            console.log(data);
+            setItem(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },[id]);
 
     const onSubmit = data => {
-        console.log(data)
+        console.log(data);
     };
 
     return (
         <div className='main-update-container container py-3'>
-            <PageTitle title='Update Item'/>
+            <PageTitle title='Update Item' />
             <div>
                 <h1 className='add-item-section-title my-3 fw-bold'>Update Inventory Item</h1>
             </div>
             <div className='manage-inventory-btn-div my-4 mx-auto  text-end'>
-               <Link to={'/manageInventories'}> <button className='manage-inventory-btn  fw-bold'><FontAwesomeIcon icon={faListCheck}/>  Manage Inventories</button></Link>
+                <Link to={'/manageInventories'}> <button className='manage-inventory-btn  fw-bold'><FontAwesomeIcon icon={faListCheck} />  Manage Inventories</button></Link>
             </div>
             <div className='update-item-div container p-4 d-flex flex-column flex-lg-row justify-content-around'>
                 <div className='update-img-div'>
-                    <img className='img-fluid mt-5' src="https://th.bing.com/th/id/OIP.2ceTwo7Xy-uUq2CC4b0sjQHaE7?w=300&h=199&c=7&r=0&o=5&dpr=1.25&pid=1.7" alt="" />
+                    <img className='img-fluid mt-5' src={item.picture} alt="" />
                 </div>
                 <div className='update-info-div text-start ms-2'>
-                    <p className='mb-2'>Id: 62742e1648832f9e44e47b62 </p>
-                    <p className='mb-2'><strong>Name:</strong> Cecelia Fischer</p>
-                    <p className='mb-2'><strong> Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, .</p>
-                    <p className='mb-2'><strong>Price:</strong> 200</p>
-                    <p className='mb-2'><strong> Stock Quantity:</strong> 20</p>
-                    <p className='mb-2'><strong>Supplier Name:</strong>  Diaz Strong</p>
+                    <p className='mb-2'>Id: {item._id} </p>
+                    <p className='mb-2'><strong>Name:</strong> {item.name}</p>
+                    <p className='mb-2'><strong> Description:</strong> {item.description}</p>
+                    <p className='mb-2'><strong>Price:</strong> {item.price}</p>
+                    <p className='mb-2'><strong> Stock Quantity:</strong> {item.quantity}</p>
+                    <p className='mb-2'><strong>Supplier Name:</strong>  {item.supplierName}</p>
                     <button className='delevered-btn'><FontAwesomeIcon icon={faShippingFast} /> Delivered</button>
 
                     <div className='restock-item-div'>
