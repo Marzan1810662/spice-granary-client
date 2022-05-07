8a577ab85f8ad230c0626240b0dce656f153d0ea
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-08 00:58:20 +0600

    get 6 items from the server on home page

--- src/components/Invenvotory/Inventory.js
+++ src/components/Invenvotory/Inventory.js
@@ -1,5 +1,6 @@
 import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Item from '../Item/Item';
@@ -9,21 +10,26 @@ const Inventory = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch('inventory.json')
-            .then(res => res.json())
-            .then(data => setItems(data))
-    }, [])
+        axios.get(`https://spice-granary.herokuapp.com/items?itemLimit=6`)
+            .then(response => {
+                const { data } = response;
+                setItems(data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
 
     return (
         <div className='container'>
             <h1 className='section-title my-4'>Inventory</h1>
             <div className='container itmes-div'>
                 {
-                    items.slice(0,6).map(item => <Item key={item._id} item={item} />)
+                    items.map(item => <Item key={item._id} item={item} />)
                 }
             </div>
             <div className='my-4'>
-               <Link to={'/manageInventories'}> <button className='manage-inventory-btn fw-bold'><FontAwesomeIcon icon={faListCheck}/>  Manage Inventories</button></Link>
+                <Link to={'/manageInventories'}> <button className='manage-inventory-btn fw-bold'><FontAwesomeIcon icon={faListCheck} />  Manage Inventories</button></Link>
             </div>
         </div>
     );
