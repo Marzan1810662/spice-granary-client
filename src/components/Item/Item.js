fda5f02b00e39f2579133ab72c35ab3989075aa5
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-08 11:03:50 +0600

    items design edited

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import './Item.css';
 
 const Item = ({ item }) => {
-    const { _id, name, email, picture, shortDescription, quantity, price, supplierName } = item;
+    const { _id, name, email, picture, description, quantity, price, supplierName } = item;
     const navigate = useNavigate();
     return (
         <div className='item-div d-flex justify-content-between'>
@@ -14,10 +14,10 @@ const Item = ({ item }) => {
             </div>
             <div className='info-div text-start'>
                 <h4>{name}</h4>
-                <p className='mb-1'> <strong>Description:</strong> {shortDescription}</p>
+                <p className='mb-1'> <strong>Description:</strong> {description}</p>
                 <p className='mb-1'><strong>Price:</strong> {price}</p>
                 <h6>Stock Quantity: {quantity}</h6>
-                <p><strong>Supplier:</strong> {supplierName}</p>
+                <p className='mb-5'><strong>Supplier:</strong> {supplierName}</p>
                 <button onClick={()=> navigate(`/inventory/${_id}`)}  className='update-btn'><FontAwesomeIcon icon={faEdit} /> Update</button>
             </div>
         </div>
